462328b72e9e0327eba51e6e9817dd55fbb4bc88
Mark <grumpycoder@outlook.com>
2019-05-17 09:56:34 -0500

    auth guard

--- APM-Start/src/app/user/login.component.ts
+++ APM-Start/src/app/user/login.component.ts
@@ -20,6 +20,12 @@ export class LoginComponent {
       this.authService.login(userName, password);
       this.router.navigate(['/products']);
 
+      if (this.authService.redirectUrl) {
+        this.router.navigateByUrl(this.authService.redirectUrl);
+      } else {
+        this.router.navigate(['/products']);
+      }
+
       // Navigate to the Product List page after log in.
     } else {
       this.errorMessage = 'Please enter a user name and password.';
