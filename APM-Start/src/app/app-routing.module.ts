494e37ae249730a8259de61a34a25594b955c33e
Mark <grumpycoder@outlook.com>
2019-05-17 10:31:50 -0500

    lazy load product module

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth.guard';
 
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
+      {
+        path: 'products',
+        canActivate: [AuthGuard],
+        loadChildren: './products/product.module#ProductModule'
+      },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', component: PageNotFoundComponent }
     ]),
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -15,7 +15,6 @@ import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 /* Feature Modules */
-import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
 
@@ -25,7 +24,6 @@ import { MessageModule } from './messages/message.module';
     BrowserAnimationsModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
-    ProductModule,
     UserModule,
     MessageModule,
     AppRoutingModule
